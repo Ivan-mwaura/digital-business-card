32db85dcaf116822abd4eac0cd12aa4e7a2b5805
Ivan-mwaura <94759966+Ivan-mwaura@users.noreply.github.com>
2022-10-30 03:48:32 +0300

    second commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import {Linkedin, Mailbox2} from 'react-bootstrap-icons'
 
 function About(){
     return(
-       <div>
-        <h1 className='about_h1'>FrontEnd web developer</h1>
-        <h1 className='about_h2'>Data analyst</h1>
+        <div>
+            <div>
+                <h1 className='about_h1'>FrontEnd web developer</h1>
+                <h1 className='about_h2'>Data analyst</h1>
+            </div>
+            <div className='button'>
+                <button className='btn_1'><Mailbox2/> Email</button>
+                <span  className='btn_2'><button className='linked_in'><Linkedin/> LinkedIn</button></span>
+            </div>
        </div>
     )
 }
--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import card from './images/card.png'
-import {Linkedin, Mailbox2} from 'react-bootstrap-icons'
 
 const Content = () => {
   return (
@@ -11,10 +10,7 @@ const Content = () => {
            <div className='title'>
                 <h1 className='heading'>Evan Mwaura</h1>
            </div>
-            <div className='button'>
-                <button className='btn_1'><Mailbox2/> Email</button>
-                <span  className='btn_2'><button className='linked_in'><Linkedin/> LinkedIn</button></span>
-            </div>
+            
         </div>
   )
 }
